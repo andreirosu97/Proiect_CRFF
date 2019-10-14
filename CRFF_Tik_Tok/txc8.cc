f93a677cf1afd3d7ffeac675b69047d8f632dd41
Andrei Rosu <andrei.rosu@veoneer.com>
2019-10-14 17:47:27 +0300

    Modified word

--- CRFF_Tik_Tok/txc8.cc
+++ CRFF_Tik_Tok/txc8.cc
@@ -1,35 +1,14 @@
-//
-// This file is part of an OMNeT++/OMNEST simulation example.
-//
-// Copyright (C) 2003-2015 Andras Varga
-//
-// This file is distributed WITHOUT ANY WARRANTY. See the file
-// `license' for details on this and other legal matters.
-//
-
 #include <stdio.h>
 #include <string.h>
 #include <omnetpp.h>
 
 using namespace omnetpp;
 
-/**
- * In the previous models, `tic' and `toc' immediately sent back the
- * received message. Here we'll add some timing: tic and toc will hold the
- * message for 1 simulated second before sending it back. In OMNeT++
- * such timing is achieved by the module sending a message to itself.
- * Such messages are called self-messages (but only because of the way they
- * are used, otherwise they are completely ordinary messages) or events.
- * Self-messages can be "sent" with the scheduleAt() function, and you can
- * specify when they should arrive back at the module.
- *
- * We leave out the counter, to keep the source code small.
- */
 class Tic8 : public cSimpleModule
 {
   private:
-    simtime_t timeout;  // timeout
-    cMessage *timeoutEvent;  // holds pointer to the timeout self-message
+    simtime_t timeout;
+    cMessage *timeoutEvent;
 
   public:
     Tic8();
@@ -58,7 +37,6 @@ void Tic8::initialize()
     timeout = 1.0;
     timeoutEvent = new cMessage("timeoutEvent");
 
-    // Generate and send initial message.
     EV << "Sending initial message\n";
     cMessage *msg = new cMessage("tictocMsg");
     send(msg, "out");
@@ -68,30 +46,22 @@ void Tic8::initialize()
 void Tic8::handleMessage(cMessage *msg)
 {
     if (msg == timeoutEvent) {
-        // If we receive the timeout event, that means the packet hasn't
-        // arrived in time and we have to re-send it.
         EV << "Timeout expired, resending message and restarting timer\n";
         cMessage *newMsg = new cMessage("tictocMsg");
         send(newMsg, "out");
         scheduleAt(simTime()+timeout, timeoutEvent);
     }
-    else {  // message arrived
-            // Acknowledgement received -- delete the received message and cancel
-            // the timeout event.
+    else {
         EV << "Timer cancelled.\n";
         cancelEvent(timeoutEvent);
         delete msg;
 
-        // Ready to send another one.
         cMessage *newMsg = new cMessage("tictocMsg");
         send(newMsg, "out");
         scheduleAt(simTime()+timeout, timeoutEvent);
     }
 }
 
-/**
- * Sends back an acknowledgement -- or not.
- */
 class Toc8 : public cSimpleModule
 {
   protected:
